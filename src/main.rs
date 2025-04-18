507d8067ab7e8de4b14aecee418fbdabcc4fcac6
mdc <galacticwingcommander@gmail.com>
2025-04-18 16:39:51 +0800

    Initial setup commit.

---
+++ src/main.rs
@@ -0,0 +1,76 @@
+// Song key shifting application.
+
+use log::info;
+use log4rs;
+use actix_files as fsx;
+use actix_web::{get, web, App, HttpServer, HttpResponse, Responder};
+use lazy_static::lazy_static;
+use std::fs;
+use std::sync::{Mutex};
+use tokio::fs::File;
+use tokio::io::AsyncReadExt;
+
+use crate::settings::Settings;
+
+pub mod settings;
+
+// Create a global variable for application settings.
+// This will be available in other files.
+lazy_static! {
+    static ref SETTINGS: Mutex<Settings> = {
+        // Read YAML settings file.
+        let mut file = futures::executor::block_on(File::open("settings.yml")).expect("Unable to open file");
+        let mut contents = String::new();
+        futures::executor::block_on(file.read_to_string(&mut contents)).expect("Unable to read file");
+
+        // Deserialize YAML into Settings struct.
+        let settings: Settings = serde_yaml::from_str(&contents).expect("Unable to parse YAML");
+        Mutex::new(settings)
+    };
+}
+
+#[get("/")]
+async fn intro() -> impl Responder {
+    HttpResponse::Ok().content_type("text/html").body(include_str!("../static/index.html"))
+}
+
+async fn help(settings: web::Data<Settings>) -> impl Responder {
+    // Help endpoint function.
+    // Read the help file.
+    let help_file_content = fs::read_to_string("./static/help.html")
+        .expect("Unable to read help file");
+
+    // Replace the version placeholder with the actual version number from settings.
+    // Repeat as necessary for other setting information required in help.
+    let help_content = help_file_content.replace("{{version}}", &settings.program_ver);
+
+    HttpResponse::Ok().content_type("text/html").body(help_content)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    // Create folders if they don't already exist.
+    fs::create_dir_all("./logs")?;
+    fs::create_dir_all("./songbits")?;
+
+    // Logging configuration held in log4rs.yml .
+    log4rs::init_file("log4rs.yml", Default::default()).unwrap();
+
+    // Get application settings in scope.
+    let settings: Settings = SETTINGS.lock().unwrap().clone();
+    // Do initial program version logging, mainly as a test.
+    info!("Application started: {} v({})", settings.program_name, settings.program_ver);
+
+    // Create and start web service.
+    HttpServer::new(move || {
+        App::new()
+            .app_data(web::Data::new(settings.clone()))
+            .service(fsx::Files::new("/songbits", "./songbits").show_files_listing())
+            .service(intro)
+            .service(actix_files::Files::new("/static", "./static").show_files_listing())
+            .route("/help", web::get().to(help))
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
+}
---
+++ src/settings.rs
@@ -0,0 +1,10 @@
+use serde::{Deserialize};
+
+#[derive(Debug, Deserialize, Clone)]
+pub struct Settings {
+    pub program_name: String,
+    pub program_ver: String,
+    pub program_devs: Vec<String>,
+    pub program_web: String,
+    pub songbits_folder: String,
+}
