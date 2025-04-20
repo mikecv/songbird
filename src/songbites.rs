28fa8b789b94487ccf1c61e6138b5712a942752b
mdc <galacticwingcommander@gmail.com>
2025-04-20 17:09:33 +0800

    Displying raw waveform, still needs styling and then extra functionality added.

--- src/songbites.rs
+++ src/songbites.rs
@@ -1,7 +1,10 @@
 // Songbite structure and methods.
 
 use log::info;
-// use log::debug;
+use log::debug;
+
+use hound::WavReader;
+use std::time::{Instant, Duration};
 
 use crate::settings::Settings;
 use crate::SETTINGS;
@@ -9,6 +12,8 @@ use crate::SETTINGS;
 // Struct of parameters for sound bite manipulation.
 pub struct Songbite {
     pub settings: Settings,
+    pub image_file: String,
+    pub load_duration: Duration,
 }
 
 // Initialise all struct variables.
@@ -22,6 +27,35 @@ impl Songbite {
 
         Songbite {
             settings: settings,
+            image_file: String::from(""),
+            load_duration: Duration::new(0, 0),
         }
     }
+}
+
+// Method to load a brand new sound file for analysis.
+impl Songbite {
+    pub fn load_new_file(&mut self, in_file:String) {
+
+        debug!("Initialising new sound file: {:?}", in_file);
+    
+        // Initialise timer for load file function.
+        let load_start = Instant::now();
+
+        // Store filename so it can be served later.
+        self.image_file = in_file.clone();
+
+        let _ = read_wav_samples(&in_file);
+
+        // Determine delta time for load function.
+        self.load_duration = load_start.elapsed();
+        info!("Time for upload: {:?}", self.load_duration)
+    }
+}
+
+// Function to load sound files for processing.
+fn read_wav_samples(path: &str) -> Result<Vec<i16>, Box<dyn std::error::Error>> {
+    let reader = WavReader::open(path)?;
+    let samples = reader.into_samples::<i16>().collect::<Result<Vec<_>, _>>()?;
+    Ok(samples)
 }
